75526859c47fc338801fe3cfe7921f200947dc0e
Oleksii Bereza <alexeybereza12345@gmail.com>
2023-07-03 16:57:46 +0300

    feat(testing): added RBAC testing

---
+++ test/RBAC.test.ts
@@ -0,0 +1,41 @@
+import { ethers } from "hardhat";
+import { Contract, BigNumber, ContractFactory, providers } from "ethers";
+import { expect } from "chai";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+
+describe("Registry contract", async () => {
+    let RBAC: Contract;
+    let RBACFactory: ContractFactory;
+
+    let alice: SignerWithAddress;
+    let bob: SignerWithAddress;
+    let charlie: SignerWithAddress;
+    let owner: SignerWithAddress;
+
+    let REBALANCE_PROVIDER_ROLE: string;
+    let AUTOCOMPOUND_PROVIDER_ROLE: string;
+
+    beforeEach(async () => {
+        REBALANCE_PROVIDER_ROLE =
+            "0x524542414c414e43455f50524f56494445525f524f4c45000000000000000000";
+        AUTOCOMPOUND_PROVIDER_ROLE =
+            "0x4155544f434f4d504f554e445f50524f56494445525f524f4c45000000000000";
+        [owner, alice, bob, charlie] = await ethers.getSigners();
+        RBACFactory = await ethers.getContractFactory("RBAC");
+        RBAC = await RBACFactory.connect(owner).deploy();
+    });
+
+    describe("Deployment", async () => {
+        it("Should return the correct rebalance provider role", async () => {
+            expect(await RBAC.REBALANCE_PROVIDER_ROLE()).to.equal(
+                REBALANCE_PROVIDER_ROLE
+            );
+        });
+
+        it("Should return the correct autocompound provider role", async () => {
+            expect(await RBAC.AUTOCOMPOUND_PROVIDER_ROLE()).to.equal(
+                AUTOCOMPOUND_PROVIDER_ROLE
+            );
+        });
+    });
+});
