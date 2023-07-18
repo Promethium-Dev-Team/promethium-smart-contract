e755591015b853472c87a1cdfa904508f1115313
Oleksii Bereza <alexeybereza12345@gmail.com>
2023-07-18 15:47:46 +0300

    added credit protocols intefaces

---
+++ contracts/interfaces/CreditProtocols/Dforce.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract dforce {
+    function borrowRatePerBlock() public view returns (uint256) {}
+
+    function supplyRatePerBlock() public view returns (uint256) {}
+}
---
+++ contracts/interfaces/CreditProtocols/Pool.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract pool {
+    function getReserveData(
+        address token
+    )
+        public
+        view
+        returns (
+            uint256,
+            uint128,
+            uint128,
+            uint128,
+            uint128,
+            uint128,
+            uint40,
+            address,
+            address,
+            address,
+            address,
+            uint8
+        )
+    {}
+}
