e07ab0b3cc112d226e210cb0026824dd3b1da8dd
Oleksii Bereza <alexeybereza12345@gmail.com>
2023-07-19 16:02:06 +0300

    feat: added new protocol intefaces

---
+++ contracts/interfaces/CreditProtocols/Borrowing.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract borrowing {
+    function borrowRate() public view returns (uint256) {}
+
+    function totalBorrows() public view returns (uint256) {}
+
+    function totalSupply() public view returns (uint256) {}
+}
