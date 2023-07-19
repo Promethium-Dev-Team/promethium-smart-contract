ca4bcdb0a2b3716241b4a08421e4d48b749a9eba
Oleksii Bereza <alexeybereza12345@gmail.com>
2023-07-19 16:01:34 +0300

    feat: added new protocols intefaces

---
+++ contracts/interfaces/CreditProtocols/Dolomite.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract dolomite {
+    function getMarketInterestRate(uint256) public view returns (uint256) {}
+
+    function getMarketTotalPar(
+        uint256
+    ) public view returns (uint256, uint256) {}
+
+    function getEarningsRate() public view returns (uint256) {}
+}
