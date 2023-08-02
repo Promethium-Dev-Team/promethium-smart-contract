9e31f2c02736caa5be972ee21c57eee9428e2829
Oleksii Bereza <alexeybereza12345@gmail.com>
2023-08-02 15:24:48 +0300

    feat: added dForce full interface

---
+++ contracts/interfaces/CreditProtocols/IDforce.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract dforce {
+    /**
+     * @dev Returns the current per-block supply interest rate.
+     *  Calculates the supply rate:
+     *  underlying = totalSupply × exchangeRate
+     *  borrowsPer = totalBorrows ÷ underlying
+     *  supplyRate = borrowRate × (1-reserveFactor) × borrowsPer
+     */
+    function supplyRatePerBlock() public view returns (uint256) {}
+
+    /**
+     * @dev Caller deposits assets into the market and caller receives iToken in exchange,
+     *        and add markets to caller's markets list for liquidity calculations.
+     * @param _mintAmount The amount of the underlying token to deposit.
+     */
+    function mintForSelfAndEnterMarket(uint256 _mintAmount) external {}
+
+    /**
+     * @dev Caller redeems specified underlying from `_from` to get underlying token.
+     * @param _from The account that would burn the iToken.
+     * @param _redeemUnderlying The number of underlying to redeem.
+     */
+    function redeemUnderlying(
+        address _from,
+        uint256 _redeemUnderlying
+    ) external {}
+
+    /**
+     * @notice Calculates interest and update total borrows and reserves.
+     * @dev Updates total borrows and reserves with any accumulated interest.
+     */
+    function updateInterest() external returns (bool) {}
+}
