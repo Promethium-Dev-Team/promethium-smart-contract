c494ca51e02cbcdc487876ad0dbd95c92e45381f
Oleksii Bereza <alexeybereza12345@gmail.com>
2023-08-07 15:53:24 +0300

    feat: added constructor args for verification

---
+++ scripts/arbitrumDeploy/arguments.ts
@@ -0,0 +1,17 @@
+module.exports = [
+    "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9",
+    "PromethiumUSDT",
+    "pUSDT",
+    "0xBAD3866bE77aedBD8C559Ad0A74dBad95161592e",
+    [
+        "0xF4B1486DD74D07706052A33d31d7c0AAFD0659E1",
+        "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9"
+    ],
+    [
+        "0xd69d402d1bdb9a2b8c3d88d98b9ceaf9e4cd72d9",
+        "0xf52f079Af080C9FB5AFCA57DDE0f8B83d49692a9",
+        "0xB65Ab7e1c6c1Ba202baed82d6FB71975D56F007C"
+    ],
+    "0x0ba2AdA8c803e85f1881E60B4Ad04C2962089956",
+    "0x0ba2AdA8c803e85f1881E60B4Ad04C2962089956"
+];
