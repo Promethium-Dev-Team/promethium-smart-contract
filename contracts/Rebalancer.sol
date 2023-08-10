8fac791868528da711a12cc9f315eb2cdf10164f
Oleksii Bereza <alexeybereza12345@gmail.com>
2023-08-10 16:43:47 +0300

    fix: comissions

--- contracts/Rebalancer.sol
+++ contracts/Rebalancer.sol
@@ -24,7 +24,7 @@ contract Rebalancer is ERC4626, Registry, ReentrancyGuard {
 
     uint64 public constant MAX_PLATFORM_FEE = 0.3 * 1e18;
     uint64 public constant MAX_WITHDRAW_FEE = 0.05 * 1e18;
-    uint256 public constant REBALANCE_THRESHOLD = 0.001 * 1e18;
+    uint256 public constant REBALANCE_THRESHOLD = 0.01 * 1e18;
     uint256 public constant WITHDRAW_QUEUE_LIMIT = 10;
     uint256 public constant feeDecimals = 18;
 
@@ -39,7 +39,11 @@ contract Rebalancer is ERC4626, Registry, ReentrancyGuard {
         address _autocompoundMatrixProvider,
         address _router
     ) ERC4626(IERC20(_asset)) ERC20(_name, _symbol) Registry(_router) {
-        FeeData = DataTypes.feeData({platformFee: 0.1 * 1e18, withdrawFee: 0.0001 * 1e18, treasury: _treasury});
+        FeeData = DataTypes.feeData({
+            platformFee: 0.1 * 1e18,
+            withdrawFee: 0.0001 * 1e18,
+            treasury: _treasury
+        });
 
         for (uint i = 0; i < _positions.length; i++) {
             addPosition(_positions[i]);
@@ -53,27 +57,25 @@ contract Rebalancer is ERC4626, Registry, ReentrancyGuard {
         grantRole(AUTOCOMPOUND_PROVIDER_ROLE, _autocompoundMatrixProvider);
     }
 
-    function _executeTransactions(DataTypes.AdaptorCall[] memory _matrix) internal {
-        for (uint8 i = 0; i < _matrix.length; ++i) {
-            address adaptor = _matrix[i].adaptor;
-            require(isAdaptorSetup[adaptor]);
-            (bool success, ) = adaptor.call(_matrix[i].callData);
-            require(success, "transaction failed");
-        }
-    }
-
     function totalAssetsWithoutFee() private view returns (uint256) {
         uint256 _totalAssets = IERC20(asset()).balanceOf(address(this));
         for (uint i = 0; i < iTokens.length; i++) {
-            _totalAssets += router.getTokenValue(asset(), iTokens[i], IERC20(iTokens[i]).balanceOf(address(this)));
+            _totalAssets += router.getTokenValue(
+                asset(),
+                iTokens[i],
+                IERC20(iTokens[i]).balanceOf(address(this))
+            );
         }
         return _totalAssets;
     }
 
     function getAvailableFee() public view returns (uint256) {
         return
-            ((totalAssetsWithoutFee() + withdrawalsAfterFeeClaim - lastBalance - depositsAfterFeeClaim) *
-                FeeData.platformFee) / (10 ** feeDecimals);
+            ((totalAssetsWithoutFee() +
+                withdrawalsAfterFeeClaim -
+                lastBalance -
+                depositsAfterFeeClaim) * FeeData.platformFee) /
+            (10 ** feeDecimals);
     }
 
     function totalAssets() public view override returns (uint256) {
@@ -84,21 +86,29 @@ contract Rebalancer is ERC4626, Registry, ReentrancyGuard {
         return super.maxWithdraw(owner) - lockedShares[owner];
     }
 
-    function harvest(DataTypes.AdaptorCall[] memory autocompoundMatrix) external nonReentrant {
+    function harvest(
+        DataTypes.AdaptorCall[] memory autocompoundMatrix
+    ) external nonReentrant {
         uint256 balanceBefore = totalAssets();
         _executeTransactions(autocompoundMatrix);
         uint256 balanceAfter = totalAssets();
-        require(balanceBefore < balanceAfter, "Balance after should be greater");
+        require(
+            balanceBefore < balanceAfter,
+            "Balance after should be greater"
+        );
 
         emit Harvest(msg.sender, balanceAfter - balanceBefore);
     }
 
-    function rebalance(DataTypes.AdaptorCall[] memory distributionMatrix) external nonReentrant {
+    function rebalance(
+        DataTypes.AdaptorCall[] memory distributionMatrix
+    ) external nonReentrant {
         uint256 balanceBefore = totalAssets();
         _executeTransactions(distributionMatrix);
         uint256 balanceAfter = totalAssets();
         require(
-            ((balanceBefore * (1e18 - REBALANCE_THRESHOLD)) / 1e18) <= balanceAfter,
+            ((balanceBefore * (1e18 - REBALANCE_THRESHOLD)) / 1e18) <=
+                balanceAfter,
             "Asset balance become too low."
         );
         for (uint256 i = 0; i < withdrawQueue.length; i++) {
@@ -118,9 +128,18 @@ contract Rebalancer is ERC4626, Registry, ReentrancyGuard {
     }
 
     function requestWithdraw(uint256 shares) public nonReentrant {
-        require(shares <= maxWithdraw(msg.sender), "ERC4626: withdraw more than max");
-        require(shares > previewWithdraw(IERC20(asset()).balanceOf(address(this))), "Instant withdraw is available");
-        require(withdrawQueue.length < WITHDRAW_QUEUE_LIMIT, "Withdraw queue limit exceeded.");
+        require(
+            shares <= maxWithdraw(msg.sender),
+            "ERC4626: withdraw more than max"
+        );
+        require(
+            shares > previewWithdraw(IERC20(asset()).balanceOf(address(this))),
+            "Instant withdraw is available"
+        );
+        require(
+            withdrawQueue.length < WITHDRAW_QUEUE_LIMIT,
+            "Withdraw queue limit exceeded."
+        );
 
         lockedShares[msg.sender] += shares;
 
@@ -132,8 +151,14 @@ contract Rebalancer is ERC4626, Registry, ReentrancyGuard {
     }
 
     function setFee(DataTypes.feeData memory newFeeData) public onlyOwner {
-        require(newFeeData.platformFee <= MAX_PLATFORM_FEE, "Platform fee limit exceeded.");
-        require(newFeeData.withdrawFee <= MAX_WITHDRAW_FEE, "Withdraw fee limit exceeded.");
+        require(
+            newFeeData.platformFee <= MAX_PLATFORM_FEE,
+            "Platform fee limit exceeded."
+        );
+        require(
+            newFeeData.withdrawFee <= MAX_WITHDRAW_FEE,
+            "Withdraw fee limit exceeded."
+        );
 
         claimFee();
         FeeData = newFeeData;
@@ -154,7 +179,12 @@ contract Rebalancer is ERC4626, Registry, ReentrancyGuard {
         super.addIToken(token);
     }
 
-    function _deposit(address caller, address receiver, uint256 assets, uint256 shares) internal virtual override {
+    function _deposit(
+        address caller,
+        address receiver,
+        uint256 assets,
+        uint256 shares
+    ) internal virtual override {
         depositsAfterFeeClaim += assets;
         super._deposit(caller, receiver, assets, shares);
     }
@@ -167,7 +197,8 @@ contract Rebalancer is ERC4626, Registry, ReentrancyGuard {
         uint256 shares
     ) internal virtual override {
         withdrawalsAfterFeeClaim += assets;
-        uint256 withdrawFee = (assets * FeeData.withdrawFee) / (10 ** feeDecimals);
+        uint256 withdrawFee = (assets * FeeData.withdrawFee) /
+            (10 ** feeDecimals);
         _payFee(withdrawFee);
         super._withdraw(caller, receiver, owner, assets - withdrawFee, shares);
     }
@@ -177,6 +208,17 @@ contract Rebalancer is ERC4626, Registry, ReentrancyGuard {
             IERC20(asset()).transfer(FeeData.treasury, amount);
 
             emit FeesCharged(FeeData.treasury, amount);
+        }
+    }
+
+    function _executeTransactions(
+        DataTypes.AdaptorCall[] memory _matrix
+    ) internal {
+        for (uint8 i = 0; i < _matrix.length; ++i) {
+            address adaptor = _matrix[i].adaptor;
+            require(isAdaptorSetup[adaptor]);
+            (bool success, ) = adaptor.call(_matrix[i].callData);
+            require(success, "transaction failed");
         }
     }
 }
