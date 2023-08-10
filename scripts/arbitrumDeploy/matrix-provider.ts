0cc0a5fd31018f70095e4dbb21993f3a77fc6cb3
Oleksii Bereza <alexeybereza12345@gmail.com>
2023-08-10 23:13:59 +0300

    feat: added a script for manual rebalance

--- scripts/arbitrumDeploy/matrix-provider.ts
+++ scripts/arbitrumDeploy/matrix-provider.ts
@@ -3,9 +3,9 @@ import { Dforce__factory, Rebalancer__factory } from "../../typechain-types";
 import { ERC20__factory } from "../../typechain-types";
 import { AAVEV3__factory } from "../../typechain-types";
 
-let rebalancerAddress: string = "0x6E0d0cFEAE705f5fC25cA6b5d7Ae3f79f13b865E";
+let rebalancerAddress: string = "0xf11650888ff3C2b2E4f653D76AD0c0fF01D86260";
 let usdtAddress: string = "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9";
-let depositAmount: string = "40000";
+let depositAmount: number = 99998;
 
 let dForceAddress: string = "0xf52f079Af080C9FB5AFCA57DDE0f8B83d49692a9";
 
@@ -29,13 +29,15 @@ async function main() {
     rebalancer.address,
     depositAmount,
   ]);
+  //data.push({ adaptor: dForce.address, callData: transaction });
+
+  transaction = dForce.interface.encodeFunctionData("redeemUnderlying", [
+    rebalancer.address,
+    depositAmount,
+  ]);
   data.push({ adaptor: dForce.address, callData: transaction });
 
-  await (
-    await rebalancer.rebalance(data, {
-      gasLimit: 7671834,
-    })
-  ).wait();
+  await (await rebalancer.rebalance(data)).wait();
 }
 
 main()
