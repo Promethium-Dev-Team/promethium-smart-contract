1a6e212787822205ab65d0d495ab786bbf48cfaa
Oleksii Bereza <alexeybereza12345@gmail.com>
2023-08-16 19:23:09 +0300

    fix: added rate function

--- contracts/interfaces/CreditProtocols/ICompound.sol
+++ contracts/interfaces/CreditProtocols/ICompound.sol
@@ -9,6 +9,13 @@ interface ICompound {
     function getUtilization() external view returns (uint);
 
     /**
+     * @dev Note: Does not accrue interest first
+     * @param utilization The utilization to check the supply rate for
+     * @return The per second supply rate at `utilization`
+     */
+    function getSupplyRate(uint utilization) external view returns (uint64);
+
+    /**
      * @notice Supply an amount of asset to the protocol
      * @param asset The asset to supply
      * @param amount The quantity to supply
