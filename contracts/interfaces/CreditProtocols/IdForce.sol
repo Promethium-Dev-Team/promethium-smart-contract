eadefc85f225fe162e37b05ebd3846d3b01e055d
Oleksii Bereza <alexeybereza12345@gmail.com>
2023-08-16 18:24:23 +0300

    feat: added dForce interface

--- contracts/interfaces/CreditProtocols/IdForce.sol
+++ contracts/interfaces/CreditProtocols/IdForce.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-contract dforce {
+interface Idforce {
     /**
      * @dev Returns the current per-block supply interest rate.
      *  Calculates the supply rate:
@@ -9,14 +9,14 @@ contract dforce {
      *  borrowsPer = totalBorrows ÷ underlying
      *  supplyRate = borrowRate × (1-reserveFactor) × borrowsPer
      */
-    function supplyRatePerBlock() public view returns (uint256) {}
+    function supplyRatePerBlock() external view returns (uint256);
 
     /**
      * @dev Caller deposits assets into the market and `_recipient` receives iToken in exchange.
      * @param _recipient The account that would receive the iToken.
      * @param _mintAmount The amount of the underlying token to deposit.
      */
-    function mint(address _recipient, uint256 _mintAmount) external {}
+    function mint(address _recipient, uint256 _mintAmount) external;
 
     /**
      * @dev Caller redeems specified underlying from `_from` to get underlying token.
@@ -26,5 +26,5 @@ contract dforce {
     function redeemUnderlying(
         address _from,
         uint256 _redeemUnderlying
-    ) external {}
+    ) external;
 }
