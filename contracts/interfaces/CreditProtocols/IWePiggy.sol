b92535ec4eade7d40980b44180195692e7e390b9
Oleksii Bereza <alexeybereza12345@gmail.com>
2023-08-16 17:18:24 +0300

    feat: added redeem function

--- contracts/interfaces/CreditProtocols/IWePiggy.sol
+++ contracts/interfaces/CreditProtocols/IWePiggy.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-contract WePiggy {
+interface WePiggy {
     /**
      * @notice Returns the current per-block supply interest rate for this cToken
      * @return The supply interest rate per block, scaled by 1e18
      */
-    function supplyRatePerBlock() external view returns (uint) {}
+    function supplyRatePerBlock() external view returns (uint);
 
     /**
      * @notice Sender supplies assets into the market and receives cTokens in exchange
@@ -14,7 +14,7 @@ contract WePiggy {
      * @param mintAmount The amount of the underlying asset to supply
      * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
      */
-    function mint(uint mintAmount) external returns (uint) {}
+    function mint(uint mintAmount) external returns (uint);
 
     /**
      * @notice Sender redeems cTokens in exchange for the underlying asset
@@ -22,12 +22,13 @@ contract WePiggy {
      * @param redeemTokens The number of cTokens to redeem into underlying
      * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
      */
-    function redeem(uint redeemTokens) external returns (uint) {}
+    function redeem(uint redeemTokens) external returns (uint);
 
     /**
-     * @notice Applies accrued interest to total borrows and reserves
-     * @dev This calculates interest accrued from the last checkpointed block
-     *   up to the current block and writes new checkpoint to storage.
+     * @notice Sender redeems cTokens in exchange for a specified amount of underlying asset
+     * @dev Accrues interest whether or not the operation succeeds, unless reverted
+     * @param redeemAmount The amount of underlying to redeem
+     * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
      */
-    function accrueInterest() public returns (uint) {}
+    function redeemUnderlying(uint redeemAmount) external returns (uint);
 }
