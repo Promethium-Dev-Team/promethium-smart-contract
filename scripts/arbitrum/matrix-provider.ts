2d74c5b8569980854e122d710dd43050a27b9719
bircher044 <alexeybereza12345@gmail.com>
2023-08-23 03:34:01 +0300

    format: prettier changes

--- scripts/arbitrum/matrix-provider.ts
+++ scripts/arbitrum/matrix-provider.ts
@@ -1,7 +1,7 @@
-import { ethers } from "hardhat";
-import { Dforce__factory, Rebalancer__factory } from "../../typechain-types";
-import { ERC20__factory } from "../../typechain-types";
-import { AAVEV3__factory } from "../../typechain-types";
+import {ethers} from "hardhat";
+import {Idforce__factory, Rebalancer__factory} from "../../typechain-types";
+import {ERC20__factory} from "../../typechain-types";
+import {AAVEV3__factory} from "../../typechain-types";
 
 let rebalancerAddress: string = "0xf11650888ff3C2b2E4f653D76AD0c0fF01D86260";
 let usdtAddress: string = "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9";
@@ -10,39 +10,30 @@ let depositAmount: number = 99998;
 let dForceAddress: string = "0xf52f079Af080C9FB5AFCA57DDE0f8B83d49692a9";
 
 async function main() {
-  const signer = (await ethers.getSigners())[2];
-  const rebalancer = Rebalancer__factory.connect(rebalancerAddress, signer);
-  const usdt = ERC20__factory.connect(usdtAddress, signer);
+    const signer = (await ethers.getSigners())[2];
+    const rebalancer = Rebalancer__factory.connect(rebalancerAddress, signer);
+    const usdt = ERC20__factory.connect(usdtAddress, signer);
 
-  let dForce = Dforce__factory.connect(dForceAddress, signer);
+    let dForce = Idforce__factory.connect(dForceAddress, signer);
 
-  const data = [];
+    const data = [];
 
-  let transaction = usdt.interface.encodeFunctionData("approve", [
-    dForce.address,
-    depositAmount,
-  ]);
+    let transaction = usdt.interface.encodeFunctionData("approve", [dForce.address, depositAmount]);
 
-  //data.push({ adaptor: usdt.address, callData: transaction });
+    //data.push({ adaptor: usdt.address, callData: transaction });
 
-  transaction = dForce.interface.encodeFunctionData("mint", [
-    rebalancer.address,
-    depositAmount,
-  ]);
-  //data.push({ adaptor: dForce.address, callData: transaction });
+    transaction = dForce.interface.encodeFunctionData("mint", [rebalancer.address, depositAmount]);
+    //data.push({ adaptor: dForce.address, callData: transaction });
 
-  transaction = dForce.interface.encodeFunctionData("redeemUnderlying", [
-    rebalancer.address,
-    depositAmount,
-  ]);
-  data.push({ adaptor: dForce.address, callData: transaction });
+    transaction = dForce.interface.encodeFunctionData("redeemUnderlying", [rebalancer.address, depositAmount]);
+    data.push({adaptor: dForce.address, callData: transaction});
 
-  await (await rebalancer.rebalance(data)).wait();
+    await (await rebalancer.rebalance(data)).wait();
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
