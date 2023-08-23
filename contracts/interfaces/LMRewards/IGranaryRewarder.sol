4e1a16f5b5817c6d4322d1359ef4a3998c1f3240
bircher044 <alexeybereza12345@gmail.com>
2023-08-23 21:33:53 +0300

    fix: naming

--- contracts/interfaces/LMRewards/IGranaryRewarder.sol
+++ contracts/interfaces/LMRewards/IGranaryRewarder.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-interface IDForceRewarder {
+interface IGranaryRewarder {
     function claimAllRewardsToSelf(
         address[] calldata assets
     ) external returns (address[] memory rewardsList, uint256[] memory claimedAmounts);
