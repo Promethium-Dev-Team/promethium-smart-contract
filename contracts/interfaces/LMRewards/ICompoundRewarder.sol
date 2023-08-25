929eb811669232a0df66528f27437ad7f87efbb6
bircher044 <alexeybereza12345@gmail.com>
2023-08-25 17:39:59 +0300

    feat: updated interface

--- contracts/interfaces/LMRewards/ICompoundRewarder.sol
+++ contracts/interfaces/LMRewards/ICompoundRewarder.sol
@@ -2,6 +2,11 @@
 pragma solidity ^0.8.0;
 
 interface ICompoundRewarder {
+    struct RewardOwed {
+        address token;
+        uint owed;
+    }
+
     /**
      * @notice Claim rewards of token type from a comet instance to owner address
      * @param comet The protocol instance
@@ -9,4 +14,11 @@ interface ICompoundRewarder {
      * @param shouldAccrue Whether or not to call accrue first
      */
     function claim(address comet, address src, bool shouldAccrue) external;
+
+    /**
+     * @notice Calculates the amount of a reward token owed to an account
+     * @param comet The protocol instance
+     * @param account The account to check rewards for
+     */
+    function getRewardOwed(address comet, address account) external returns (RewardOwed memory);
 }
