9bb9e5da9510cd372e009ed080ca086dc8909d40
bircher044 <alexeybereza12345@gmail.com>
2023-08-31 20:14:11 +0300

    fix: withdrawal event duplicates

--- contracts/Rebalancer.sol
+++ contracts/Rebalancer.sol
@@ -10,7 +10,7 @@ contract Rebalancer is ERC4626, Registry, ReentrancyGuard {
     event FeesChanged(address owner, DataTypes.feeData newFeeData);
     event FeesCharged(address treasury, uint256 amount);
     event RequestWithdraw(address withdrawer, uint256 shares, uint256 id);
-    event WithdrawalCompleted(address withdrawer, uint256 amount, uint256 id);
+    event WithdrawRequested(address indexed withdrawer, uint256 assets, uint256 shares);
     event SetPoolLimit(uint256 newLimit);
 
     DataTypes.feeData public FeeData;
@@ -214,6 +214,22 @@ contract Rebalancer is ERC4626, Registry, ReentrancyGuard {
         super._withdraw(caller, receiver, owner, assets - withdrawFee, shares);
     }
 
+    function _withdrawRequested(
+        address withdrawer,
+        uint256 assets,
+        uint256 shares
+    ) internal {
+        withdrawalsAfterFeeClaim += assets;
+        uint256 withdrawFee = (assets * FeeData.withdrawFee) / (10 ** feeDecimals);
+        _payFee(withdrawFee);
+
+        assets -= withdrawFee;
+        _burn(withdrawer, shares);
+        SafeERC20.safeTransfer(IERC20(asset()), withdrawer, assets);
+
+        emit WithdrawRequested(withdrawer, assets, shares);
+    }
+
     /**
      * @notice  an internal function to take collected performance fee
      */
@@ -244,14 +260,11 @@ contract Rebalancer is ERC4626, Registry, ReentrancyGuard {
         for (uint256 i = 0; i < withdrawQueue.length; i++) {
             lockedShares[withdrawQueue[i].receiver] -= withdrawQueue[i].shares;
             uint256 assets = convertToAssets(withdrawQueue[i].shares);
-            _withdraw(
-                withdrawQueue[i].receiver,
-                withdrawQueue[i].receiver,
+            _withdrawRequested(
                 withdrawQueue[i].receiver,
                 assets,
                 withdrawQueue[i].shares
             );
-            emit WithdrawalCompleted(withdrawQueue[i].receiver, assets, withdrawQueue[i].id);
         }
         delete withdrawQueue;
         totalRequested = 0;
